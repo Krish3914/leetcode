24ed04f67db396a8065a6262aafc3e9801cfa570
Krish3914 <105340895+Krish3914@users.noreply.github.com>
2023-10-22 05:13:15 +0000

    new

--- move_zeroes.cpp
+++ move_zeroes.cpp
@@ -4,21 +4,39 @@ static const int __ = []() {
   std::cout.tie(NULL);
   return 0;
 }();
+// class Solution {
+// public:
+//   void moveZeroes(vector<int> &nums) {
+//     int i;
+//     int n = nums.size();
+//     vector<int> result;
+//     for (i = n - 1; i >= 0; i--) {
+//       if (nums[i] == 0) {
+//         result.erase(result.begin() + i);
+//         // i--;
+//         result.push_back(0);
+//       } else {
+//         continue;
+//       }
+//     }
+//     nums = result;
+//   }
+// };
 class Solution {
 public:
   void moveZeroes(vector<int> &nums) {
-    int i;
-    int n = nums.size();
     vector<int> result;
-    for (i = n - 1; i >= 0; i--) {
-      if (nums[i] == 0) {
-        result.erase(result.begin() + i);
-        // i--;
-        result.push_back(0);
-      } else {
-        continue;
+
+    for (int i = 0; i < nums.size(); i++) {
+      if (nums[i] != 0) {
+        result.push_back(nums[i]);
       }
     }
+
+    while (result.size() != nums.size()) {
+      result.push_back(0);
+    }
+
     nums = result;
   }
 };
