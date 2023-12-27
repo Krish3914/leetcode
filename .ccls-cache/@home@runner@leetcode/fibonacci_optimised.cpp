f6d254604998b0b652a72ebf8700d45bf90b7078
Krish3914 <105340895+Krish3914@users.noreply.github.com>
2023-12-27 10:03:51 +0000

    27dec2023

--- .ccls-cache/@home@runner@leetcode/fibonacci_optimised.cpp
+++ .ccls-cache/@home@runner@leetcode/fibonacci_optimised.cpp
@@ -1,4 +1,4 @@
-// // #include <bits/stdc++.h> 
+// // #include <bits/stdc++.h>
 // // int fibonacciNumber(int n){
 // //     // Write your code here.
 // //     // int t;
@@ -14,7 +14,8 @@
 // //     // int count = 2;
 // //     int i;
 // //     for(i=2; i<n; i++){
-// //         arr.push_back((arr[i-1]%1000000007 + arr[i-2]%1000000007)%1000000007);
+// //         arr.push_back((arr[i-1]%1000000007 +
+// arr[i-2]%1000000007)%1000000007);
 // //     }
 // //     // cout<<arr[n-1]<<endl;
 // //     // t--;}
@@ -37,7 +38,7 @@
 // 	}
 
 // 	// Recursive call.
-// 	return (fibonacciNumber(n - 1) + fibonacciNumber(n - 2)) % mod;    
+// 	return (fibonacciNumber(n - 1) + fibonacciNumber(n - 2)) % mod;
 // }
 /*
     Time Complexity: O(log(N)).
@@ -47,53 +48,54 @@
 */
 
 // Function to multiply two matrices.
-vector<vector<int>> multiply(vector<vector<int>> a,vector<vector<int>> b){
+vector<vector<int>> multiply(vector<vector<int>> a, vector<vector<int>> b) {
 
-    const int mod = 1e9 + 7;
+  const int mod = 1e9 + 7;
 
-    // To store the resultant matrix.
-    vector<vector<int>> c(2,vector<int>(2,0));
+  // To store the resultant matrix.
+  vector<vector<int>> c(2, vector<int>(2, 0));
 
-    for(int i = 0; i < 2; i++){
-        for(int j = 0; j < 2; j++){
-            for(int k = 0; k < 2; k++){
-                c[i][k] = (c[i][k] + (long long)((long long)a[i][j] * (long long)b[j][k])) % mod;
-            }
-        }
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 2; j++) {
+      for (int k = 0; k < 2; k++) {
+        c[i][k] =
+            (c[i][k] + (long long)((long long)a[i][j] * (long long)b[j][k])) %
+            mod;
+      }
     }
+  }
 
-    return c;
+  return c;
 }
 
 // Function to calclate n'th power of a matrix.
-vector<vector<int>> matrix_power(vector<vector<int>> coef ,int n){
+vector<vector<int>> matrix_power(vector<vector<int>> coef, int n) {
 
-    // To store the resultant matrix.
-    vector<vector<int>> res = {{1,0},{0,1}};
+  // To store the resultant matrix.
+  vector<vector<int>> res = {{1, 0}, {0, 1}};
 
-    // While loop till n > 0.
-    while(n){
-        // If n is odd, multiply res with coef.
-        if(n % 2) res = multiply(res,coef);
+  // While loop till n > 0.
+  while (n) {
+    // If n is odd, multiply res with coef.
+    if (n % 2)
+      res = multiply(res, coef);
 
-        // Multiply coef with coef and update n to n/2.
-        coef = multiply(coef,coef);
-        n /= 2;
-    }
+    // Multiply coef with coef and update n to n/2.
+    coef = multiply(coef, coef);
+    n /= 2;
+  }
 
-    return res;
-
+  return res;
 }
 
+int fibonacciNumber(int n) {
 
-int fibonacciNumber(int n){
+  // Declaring the coef matrix.
+  vector<vector<int>> coef = {{0, 1}, {1, 1}};
 
-    // Declaring the coef matrix.
-    vector<vector<int>> coef={{0,1},{1,1}};
+  // Calculating the (n-1)th power of the coef matrix.
+  vector<vector<int>> res = matrix_power(coef, n - 1);
 
-    // Calculating the (n-1)th power of the coef matrix.
-    vector<vector<int>> res = matrix_power(coef,n-1);
-
-    // Return the bottom right element of the resultant matrix.
-    return res[1][1];
+  // Return the bottom right element of the resultant matrix.
+  return res[1][1];
 }
