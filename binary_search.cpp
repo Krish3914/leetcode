d3d7db90647d76478202638bff4902d1546114b1
Krish3914 <105340895+Krish3914@users.noreply.github.com>
2024-01-14 15:33:57 +0000

    14Jan2024

---
+++ binary_search.cpp
@@ -0,0 +1,43 @@
+static const int __ = []() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(NULL);
+  std::cout.tie(NULL);
+  return 0;
+}();
+class Solution {
+public:
+  // void binarysearch(int &low, int &high, vector<int>& nums, int &target){
+  //     int mid = low + (high-low)/2;
+  //     if(target==nums[mid]){
+  //         return;
+  //     }
+  //     else if(target<nums[mid]){
+  //         high = mid;
+  //         binarysearch(low,high,nums,target);
+  //     }
+  //     else{
+  //         low = mid;
+  //         binarysearch(low,high,nums,target);
+  //     }
+  // }
+  int search(vector<int> &nums, int target) {
+    int n = nums.size();
+    int low = 0;
+    int high = n - 1;
+    int mid;
+    while (low <= high) {
+      mid = low + (high - low) / 2;
+      if (nums[mid] == target) {
+        return mid;
+      } else if (target < nums[mid]) {
+        high = mid - 1;
+      } else {
+        low = mid + 1;
+      }
+    }
+    // int index;
+    // binarysearch(low,high,nums,target);
+    // index = low + (high-low)/2;
+    return -1;
+  }
+};
---
+++ check_if_stringareclose.cpp
@@ -0,0 +1,38 @@
+static const int __ = []() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(NULL);
+  std::cout.tie(NULL);
+  return 0;
+}();
+class Solution {
+public:
+  bool closeStrings(string word1, string word2) {
+    if (word1.size() != word2.size()) {
+      return false;
+    }
+    int i, j;
+    int n1 = word1.size();
+    int n2 = word2.size();
+    unordered_map<char, int> mp1, mp2;
+    for (i = 0; i < n1; i++) {
+      mp1[word1[i]]++;
+    }
+    for (i = 0; i < n2; i++) {
+      mp2[word2[i]]++;
+    }
+    vector<int> ar1, ar2, arr1, arr2;
+    for (auto k : mp1) {
+      arr1.push_back(k.first);
+      ar1.push_back(k.second);
+    }
+    for (auto k : mp2) {
+      arr2.push_back(k.first);
+      ar2.push_back(k.second);
+    }
+    sort(ar1.begin(), ar1.end());
+    sort(ar2.begin(), ar2.end());
+    sort(arr2.begin(), arr2.end());
+    sort(arr1.begin(), arr1.end());
+    return (ar1 == ar2 && arr1 == arr2);
+  }
+};
