18bcf5c9fd807acf17e5aef89d0226e2e71b2617
Krish Agrawal <105340895+Krish3914@users.noreply.github.com>
2024-05-10 14:40:44 +0000

    10May2024

--- 2sum.cpp
+++ 2sum.cpp
@@ -5,6 +5,61 @@ static const int __ = []() {
   return 0;
 }();
 
+// optimised approach
+static const int __ = []() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(NULL);
+  std::cout.tie(NULL);
+  return 0;
+}();
+
+class Solution {
+public:
+  vector<int> twoSum(vector<int> &nums, int target) {
+    int i, j;
+    unordered_map<int, int> mp;
+    for (i = 0; i < nums.size(); i++) {
+      mp[nums[i]]++;
+    }
+    vector<int> arr;
+    for (i = 0; i < nums.size(); i++) {
+      if (nums[i] != target - nums[i]) {
+        if (mp[nums[i]] > 0 && (mp[target - nums[i]]) > 0) {
+          for (j = 0; j < nums.size(); j++) {
+            if (nums[j] == nums[i]) {
+              arr.push_back(j);
+              break;
+            }
+          }
+          for (j = 0; j < nums.size(); j++) {
+            if (nums[j] == target - nums[i]) {
+              arr.push_back(j);
+              break;
+            }
+          }
+          return arr;
+        } else {
+          continue;
+        }
+      } else {
+        if (mp[nums[i]] < 2) {
+          continue;
+        } else {
+          for (j = 0; j < nums.size(); j++) {
+            if (nums[i] == nums[j]) {
+              arr.push_back(j);
+            }
+            if (arr.size() == 2) {
+              return arr;
+            }
+          }
+        }
+      }
+    }
+    return arr;
+  }
+};
+
 // class Solution {
 // public:
 //     vector<int> twoSum(vector<int>& nums, int target) {
@@ -46,24 +101,24 @@ static const int __ = []() {
 //     }
 // };
 
-class Solution {
-public:
-  vector<int> twoSum(vector<int> &nums, int target) {
-    int i, n = nums.size(), y, j;
-    vector<int> arr(2);
-    for (i = 0; i < n; i++) {
-      y = (target - nums[i]);
-      for (j = i + 1; j < n; j++) {
-        if (nums[j] == y) {
-          arr[0] = i;
-          arr[1] = j;
-          break;
-        }
-      }
-      if (arr[0] != arr[1]) {
-        break;
-      }
-    }
-    return arr;
-  }
-};
+// class Solution {
+// public:
+//   vector<int> twoSum(vector<int> &nums, int target) {
+//     int i, n = nums.size(), y, j;
+//     vector<int> arr(2);
+//     for (i = 0; i < n; i++) {
+//       y = (target - nums[i]);
+//       for (j = i + 1; j < n; j++) {
+//         if (nums[j] == y) {
+//           arr[0] = i;
+//           arr[1] = j;
+//           break;
+//         }
+//       }
+//       if (arr[0] != arr[1]) {
+//         break;
+//       }
+//     }
+//     return arr;
+//   }
+// };
